7a452c9185d67c6d04538fcf9435607cbc466830
jrobles2299 <94424567+jrobles2299@users.noreply.github.com>
2022-05-30 20:59:50 -0600

    a

--- Ej_15/Receta.cs
+++ Ej_15/Receta.cs
@@ -5,8 +5,9 @@ using System.Web;
 
 namespace Ej_15
 {
-    public class Receta : Medicamentos
+    public class Receta 
     {
+        public Medicamentos Medicamento { get; set; }
         public float Dosis { get; set; }
         public string HorasAdministradas { get; set; }
     }
